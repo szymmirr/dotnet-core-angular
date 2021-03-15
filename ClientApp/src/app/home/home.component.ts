fad3da5ec1d1699b9103c8170b9e8ba22460c990
szymmirr <szymmirr@wp.pl>
2021-03-15 01:26:23 +0100

    Add new home screen

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -5,4 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  public szczecin = require("./szczecin.jpg");
+  public szczecinLeft = require("./szczecin-left.jpg");
+  public szczecin1 = require("./szczecin1.jpg");
+  public szczecin2 = require("./szczecin2.jpg");
+  public szczecin3 = require("./szczecin3.jpg");
+  public szczecin4 = require("./szczecin4.jpg");
+  public szczecin5 = require("./szczecin5.png");
+  public szczecin6 = require("./szczecin6.png");
+  public szczecin7 = require("./szczecin7.png");
 }
